cbd1c0cc562f3f0930cf2780fa1c679a92b32bf2
SayanQi <sayan.dey@quantuminventions.com>
2024-01-29 12:35:18 +0530

    Enhance: [R-105300] added 3 questions

---
+++ String/Third.rb
@@ -0,0 +1,15 @@
+#Question : Write a Ruby program to count the occurrences of a specified character in a given string.
+#get input from user
+puts "Enter a string"
+str = gets.chomp
+
+#get char to be find
+puts "Enter a string to find occurrences"
+char = gets.chomp
+
+#check occurences
+if (str.count char) > 0
+  puts "#{char} occur in #{str} #{str.count char} times!"
+else
+  puts "No #{char} in #{str}"
+end
