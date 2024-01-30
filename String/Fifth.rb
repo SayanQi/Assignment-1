01350fd2eaaa77b7ce77e71ad5225aaf53949cb3
SayanQi <sayan.dey@quantuminventions.com>
2024-01-30 18:11:57 +0530

    testing ssh

--- String/Fifth.rb
+++ String/Fifth.rb
@@ -1,13 +1,13 @@
 #Question : Write a Ruby program to replace a substring from a specified string.
 #define function for this pgrm
-def replaceString (str,targetStr,replaceStr)
+def replaceString (str, targetStr, replaceStr)
   #check any empty string occurred
-  if str.length ==0 || targetStr.length==0 || replaceStr.length==0
+  if str.length == 0 || targetStr.length == 0 || replaceStr.length == 0
     return "Null String"
   #check target string is included in str or not
   elsif str.include? targetStr
     #replace strings
-    return str.gsub(targetStr,replaceStr)
+    return str.gsub(targetStr, replaceStr)
   else
     return "Target String Not Found"
   end
@@ -16,11 +16,14 @@ end
 #get input from user
 puts "Enter a string"
 str = gets.chomp
+
 #get string to be replaced
 puts "Enter a string to be replaced"
 targetStr = gets.chomp
+
 #get string to replace
 puts "Enter a string to replace"
 replaceStr = gets.chomp
+
 #check if target exist
-puts replaceString(str,targetStr,replaceStr)
+puts replaceString(str, targetStr, replaceStr)
--- String/first.rb
+++ String/first.rb
@@ -1,13 +1,17 @@
 # Question : Write a Ruby program to lower case, upper case and capitalize all the words of a given string.
-#get input from user
+# get input from user
 puts "Enter a string"
 str = gets.chomp
-#to uppercase the string
+
+# to uppercase the string
 upStr = str.upcase
-#to lowercase the string
+
+# to lowercase the string
 dnStr = str.downcase
-#to capitalize the string
+
+# to capitalize the string
 capStr = str.capitalize
+
 puts "#{str} uppercase -> #{upStr}"
 puts "#{str} downase -> #{dnStr}"
 puts "#{str} capitalized -> #{capStr}"
