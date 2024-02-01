85aa15c4fd69486ab73fc9b54cbd008134734864
SayanQi <sayan.dey@quantuminventions.com>
2024-02-01 16:15:06 +0530

    changes done

--- oops/Fifth.rb
+++ oops/Fifth.rb
@@ -1,33 +1,19 @@
 # Write a program to create a nested module.
 
 module Mobile
-  @android = 11
-
-  @android.freeze
-  # create a nested module
-  module SmartPhone
+  ANDROID = 11.freeze
 
     # defined a fn to return android version
-    def android_version
-      @android
+    def self.android_version
+     ANDROID
     end
 
     # define a setter
-    def set_android_version(an)
-      @android = an
+    def self.check_frozen_android
+      ANDROID.frozen?
     end
 
-  end
-
 end
 
-class Fifth
 
-  include Mobile::SmartPhone
-
-end
-
-obj = Fifth.new
-obj.freeze
-obj.set_android_version(1.23)
-puts obj.android_version
+puts Mobile.check_frozen_android
