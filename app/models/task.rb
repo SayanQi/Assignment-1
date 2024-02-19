170c43fe436aa41377a65166da080bed84bcf671
SayanQi <sayan.dey@quantuminventions.com>
2024-02-19 18:15:02 +0530

    indentation fixed, unused codes removed

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,11 +1,5 @@
 class Task < ApplicationRecord
-  # after_create -> { puts " created this!!!!!!!!!#{self.id}" }
-  after_create :xyz
   validates :name, presence: true
   validates :description, presence: true
   validates :pending_date, presence: true
-
-  def xyz
-    puts ("this is xyz metod!!!!!!!")
-  end
 end
