9b080acf6c03c844530115e8c5b2c7599778d8bb
SayanQi <sayan.dey@quantuminventions.com>
2024-02-23 10:34:17 +0530

    overdue issue added!

--- app/javascript/controllers/hello_controller.js
+++ app/javascript/controllers/hello_controller.js
@@ -3,11 +3,5 @@ import { Controller } from "stimulus";
 import { Turbo } from "@hotwired/turbo-rails";
 
 export default class extends Controller {
-  // Action to reload the Turbo Frame
-  reload() {
-    console.log("helo cont");
-    console.log(this.element);
-    const turboFrame = this.element;
-    Turbo.visit(turboFrame.src, { action: "replace" }); // Reload the Turbo Frame
-  }
+ 
 }
